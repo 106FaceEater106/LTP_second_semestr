3a1d78964b77f36409b7f9d1aae095b2ed0d93ff
Hast <43549217+hastyulia@users.noreply.github.com>
2019-05-06 14:58:16 +0500

    Add files via upload

---
+++ 11-12. Game Sapper/SapperTest/SapperTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using saper;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CreateField()
+        {
+            new Field(10, 15);
+        }
+
+        [TestMethod]
+        public void FieldLenght()
+        {
+            var f = new Field(10, 15);
+            Assert.AreEqual(10, f.N);
+        }
+
+        [TestMethod]
+        public void FieldLevelsName()
+        {
+            var f = new Field(10, 15);
+            var level = new LevelEl { name = "Easy", percent = 15 };
+            Assert.AreEqual(level.name, f.levels[0].name);
+        }
+
+
+        [TestMethod]
+        public void FieldMinAround()
+        {
+            var f = new Field(10, 15);
+            int res = 0;
+            for (int k = 0; k < f.N; k++)
+            {
+                for (int t = 0; t < f.N; t++)
+                {
+                    if (Math.Abs(2 - k) <= 1 && Math.Abs(t - 3) <= 1 && f.cells[k, t].HasMine)
+                    {
+                        res++;
+                    }
+                }
+            }
+            Assert.AreEqual(res, f.MinesAround(2, 3));
+        }
+
+    }
+}
